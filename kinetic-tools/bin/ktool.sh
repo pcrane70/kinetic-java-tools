60785e05bd1e3394b8c3294b5e02284da61d4976
lichenchong <lichenchong9@gmail.com>
2015-05-19 12:24:52 -0700

    Add "debug mode" switch for kinetic-tools, default switch is off. The usage is as following: -Dkinetic.debug=true
    For instance: 
    sh ktool.sh -getlog -in drive17 -Dkinetic.debug=true

--- kinetic-tools/bin/ktool.sh
+++ kinetic-tools/bin/ktool.sh
@@ -2,7 +2,6 @@
 
 BASE_DIR=`dirname "$0"`/..
 BASE_DIR=`cd "$BASE_DIR"; pwd`
-#echo "BASE_DIR=$BASE_DIR"
 DEBUG=false
 
 JAVA=""
@@ -25,7 +24,21 @@ for f in $BASE_DIR/target/*.jar; do
    CLASSPATH=${CLASSPATH}:$f
 done
 
-#echo "CLASSPATH=$CLASSPATH"
+function getName() {
+	for item in $@ ; do
+		if [[ "$item" = "-Dkinetic.debug=true" ]]; then
+			DEBUG=true;
+		fi
+	done
+}
 
-exec "$JAVA" -DKINETIC_TOOLS_HOME="$BASE_DIR" -classpath "$CLASSPATH" com.seagate.kinetic.tools.management.cli.KineticToolCLI "$@"
+getName $@
+
+if [[ "$DEBUG" = "true" ]]; then
+    echo "Turn on kinetic-tools debug mode: "
+    exec "$JAVA" -DKINETIC_TOOLS_HOME="$BASE_DIR" -classpath "$CLASSPATH" -Dkinetic.io.in=true -Dkinetic.io.out=true com.seagate.kinetic.tools.management.cli.KineticToolCLI "${@/-Dkinetic.debug*}"
+else
+    exec "$JAVA" -DKINETIC_TOOLS_HOME="$BASE_DIR" -classpath "$CLASSPATH" com.seagate.kinetic.tools.management.cli.KineticToolCLI "${@/-Dkinetic.debug*}"
+fi
+
 
