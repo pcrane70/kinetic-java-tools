18fc64373560d5bf1321eea4fbd1072ef687ce21
muhammad shafiq <muhammad.shafiq@seagate.com>
2015-07-17 15:43:24 -0700

    Added Sample configuration output

--- README.swift
+++ README.swift
@@ -37,6 +37,337 @@ curl -d '{"msg":"container"}' http://localhost:8080/external?class=Ring
 curl -d '{"msg":"object"}' http://localhost:8080/external?class=Ring
 
 ============================  SAMPLE OUTPUT========================
+
+curl -d '{"msg":"proxy"}' http://localhost:8080/external?class=Config
+# /etc/swift/proxy-server.conf
+[filter:cname_lookup]
+bind_port = 8080
+use = egg:swift#cname_lookup
+user = mshafiq
+
+[filter:tempauth]
+use = egg:swift#tempauth
+user_test2_tester2 = testing2 .admin
+bind_port = 8080
+user_test_tester = testing .admin
+user_test_tester3 = testing3
+user_admin_admin = admin .admin .reseller_admin
+user_test5_tester5 = testing5 service
+user = mshafiq
+
+[filter:proxy-logging]
+bind_port = 8080
+use = egg:swift#proxy_logging
+user = mshafiq
+
+[filter:bulk]
+bind_port = 8080
+use = egg:swift#bulk
+user = mshafiq
+
+[filter:healthcheck]
+bind_port = 8080
+use = egg:swift#healthcheck
+user = mshafiq
+
+[filter:cache]
+bind_port = 8080
+use = egg:swift#memcache
+user = mshafiq
+
+[filter:xprofile]
+bind_port = 8080
+use = egg:swift#xprofile
+user = mshafiq
+object_single_process = object-server.conf
+
+[filter:staticweb]
+bind_port = 8080
+use = egg:swift#staticweb
+user = mshafiq
+
+[filter:tempurl]
+bind_port = 8080
+use = egg:swift#tempurl
+user = mshafiq
+
+[filter:list-endpoints]
+bind_port = 8080
+use = egg:swift#list_endpoints
+user = mshafiq
+
+[filter:dlo]
+bind_port = 8080
+use = egg:swift#dlo
+user = mshafiq
+
+[filter:account-quotas]
+bind_port = 8080
+use = egg:swift#account_quotas
+user = mshafiq
+
+[filter:gatekeeper]
+bind_port = 8080
+use = egg:swift#gatekeeper
+user = mshafiq
+
+[filter:container_sync]
+bind_port = 8080
+use = egg:swift#container_sync
+user = mshafiq
+
+[pipeline:main]
+bind_port = 8080
+pipeline = catch_errors gatekeeper healthcheck proxy-logging cache container_sync bulk tempurl ratelimit tempauth container-quotas account-quotas slo dlo proxy-logging proxy-server
+user = mshafiq
+
+[filter:name_check]
+bind_port = 8080
+use = egg:swift#name_check
+user = mshafiq
+
+[filter:domain_remap]
+bind_port = 8080
+use = egg:swift#domain_remap
+user = mshafiq
+
+[filter:slo]
+bind_port = 8080
+use = egg:swift#slo
+user = mshafiq
+
+[filter:ratelimit]
+bind_port = 8080
+use = egg:swift#ratelimit
+user = mshafiq
+
+[filter:catch_errors]
+bind_port = 8080
+use = egg:swift#catch_errors
+user = mshafiq
+
+[app:proxy-server]
+bind_port = 8080
+use = egg:swift#proxy
+user = mshafiq
+account_autocreate = true
+
+[filter:container-quotas]
+bind_port = 8080
+use = egg:swift#container_quotas
+user = mshafiq
+
+[filter:formpost]
+bind_port = 8080
+use = egg:swift#formpost
+user = mshafiq
+
+# __file__ = /etc/swift/proxy-server.conf
+# log_name = None
+-------------------------------------------------------------
+
+curl -d '{"msg":"account"}' http://localhost:8080/external?class=Config
+
+# /etc/swift/account-server.conf
+[app:account-server]
+bind_port = 6002
+mount_check = false
+use = egg:swift#account
+user = mshafiq
+devices = /swift
+
+[account-reaper]
+bind_port = 6002
+mount_check = false
+user = mshafiq
+devices = /swift
+
+[account-replicator]
+bind_port = 6002
+mount_check = false
+user = mshafiq
+devices = /swift
+
+[filter:xprofile]
+bind_port = 6002
+mount_check = false
+use = egg:swift#xprofile
+user = mshafiq
+devices = /swift
+
+[account-auditor]
+bind_port = 6002
+mount_check = false
+user = mshafiq
+devices = /swift
+
+[filter:healthcheck]
+bind_port = 6002
+mount_check = false
+use = egg:swift#healthcheck
+user = mshafiq
+devices = /swift
+
+[filter:recon]
+bind_port = 6002
+mount_check = false
+use = egg:swift#recon
+user = mshafiq
+devices = /swift
+
+[pipeline:main]
+bind_port = 6002
+mount_check = false
+pipeline = healthcheck account-server
+user = mshafiq
+devices = /swift
+
+# __file__ = /etc/swift/account-server.conf
+# log_name = None
+
+---------------------------------------------------------------
+
+curl -d '{"msg":"container"}' http://localhost:8080/external?class=Config
+
+# /etc/swift/container-server.conf
+[filter:healthcheck]
+bind_port = 6001
+mount_check = false
+use = egg:swift#healthcheck
+user = mshafiq
+devices = /swift
+
+[container-auditor]
+bind_port = 6001
+mount_check = false
+user = mshafiq
+devices = /swift
+
+[app:container-server]
+bind_port = 6001
+mount_check = false
+use = egg:swift#container
+user = mshafiq
+devices = /swift
+
+[filter:xprofile]
+bind_port = 6001
+mount_check = false
+use = egg:swift#xprofile
+user = mshafiq
+devices = /swift
+
+[filter:recon]
+bind_port = 6001
+mount_check = false
+use = egg:swift#recon
+user = mshafiq
+devices = /swift
+
+[container-updater]
+bind_port = 6001
+mount_check = false
+user = mshafiq
+devices = /swift
+
+[container-replicator]
+bind_port = 6001
+mount_check = false
+user = mshafiq
+devices = /swift
+
+[pipeline:main]
+bind_port = 6001
+mount_check = false
+pipeline = healthcheck container-server
+user = mshafiq
+devices = /swift
+
+[container-sync]
+bind_port = 6001
+mount_check = false
+user = mshafiq
+devices = /swift
+
+# __file__ = /etc/swift/container-server.conf
+# log_name = None
+-----------------------------------------------------------------
+curl -d '{"msg":"object"}' http://localhost:8080/external?class=Config
+# /etc/swift/object-server.conf
+[object-replicator]
+bind_port = 6000
+mount_check = false
+disk_chunk_size = 1048576
+user = mshafiq
+devices = /swift
+
+[app:object-server]
+disk_chunk_size = 1048576
+use = egg:swift#object
+bind_port = 6000
+mount_check = false
+devices = /swift
+user = mshafiq
+
+[pipeline:main]
+disk_chunk_size = 1048576
+pipeline = healthcheck object-server
+bind_port = 6000
+mount_check = false
+devices = /swift
+user = mshafiq
+
+[filter:xprofile]
+disk_chunk_size = 1048576
+use = egg:swift#xprofile
+bind_port = 6000
+mount_check = false
+devices = /swift
+user = mshafiq
+
+[filter:recon]
+disk_chunk_size = 1048576
+use = egg:swift#recon
+bind_port = 6000
+mount_check = false
+devices = /swift
+user = mshafiq
+
+[object-updater]
+bind_port = 6000
+mount_check = false
+disk_chunk_size = 1048576
+user = mshafiq
+devices = /swift
+
+[object-auditor]
+bind_port = 6000
+mount_check = false
+disk_chunk_size = 1048576
+user = mshafiq
+devices = /swift
+
+[filter:healthcheck]
+disk_chunk_size = 1048576
+use = egg:swift#healthcheck
+bind_port = 6000
+mount_check = false
+devices = /swift
+user = mshafiq
+
+[object-reconstructor]
+bind_port = 6000
+mount_check = false
+disk_chunk_size = 1048576
+user = mshafiq
+devices = /swift
+
+# __file__ = /etc/swift/object-server.conf
+# log_name = None
+
+
+
+
 curl -d '{"msg":"account"}' http://localhost:8080/external?class=Ring
 account.builder, build version 1
 1024 partitions, 3.000000 replicas, 1 regions, 1 zones, 1 devices, 0.00 balance, 0.00 dispersion
