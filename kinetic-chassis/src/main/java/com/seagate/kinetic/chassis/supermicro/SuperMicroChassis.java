1a243ca1cff79aae32380a45dbf30c9434dbeb5c
chiaming2000 <chiaming2000@gmail.com>
2015-09-20 16:55:57 -0700

    Kinetic chassis interface development project. Each vendor specific management interface should be placed under its own package.

---
+++ kinetic-chassis/src/main/java/com/seagate/kinetic/chassis/supermicro/SuperMicroChassis.java
@@ -0,0 +1,14 @@
+package com.seagate.kinetic.chassis.supermicro;
+
+/**
+ * Template holder for super micro chassis monitoring and management.
+ *
+ */
+public class SuperMicroChassis 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("Hello Super Chassis!");
+    }
+}
---
+++ kinetic-chassis/src/main/java/com/seagate/kinetic/chassis/titan/TitanChasis.java
@@ -0,0 +1,14 @@
+package com.seagate.kinetic.chassis.titan;
+
+/**
+ * Template holder for super micro chassis monitoring and management.
+ *
+ */
+public class TitanChasis 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("Hello Titan Chassis!");
+    }
+}
