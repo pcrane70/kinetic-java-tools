d4dbf56d504e4f65d58276bad98992e001f8cb01
lichenchong <lichenchong9@gmail.com>
2015-10-20 12:04:55 -0700

    Add one simple example console runner for the beginner, please read the readme.txt in kinetic-console/config/example/readme.txt to see how to run.

---
+++ kinetic-console/config/example/readme.txt
@@ -0,0 +1,15 @@
+How to use the example
+======================================
+1. Copy the content of simulator_manifest.json (JSON file) to ```kinetic-java-tools/kinetic-console/config/default/hwview_default.json```
+
+2. cd kinetic-console 
+
+3. mvn clean package
+
+4. cd bin
+   
+5. sh startExampleSimulator.sh
+   
+6. sh startKineticConsole.sh
+
+7. open the URL "http://localhost:8080/kinetic/console" with a browser
---
+++ kinetic-console/src/test/java/com/seagate/kinetic/console/example/SimulatorDrives.java
@@ -0,0 +1,122 @@
+/**
+ * 
+ * Copyright (C) 2014 Seagate Technology.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+package com.seagate.kinetic.console.example;
+
+import java.io.File;
+
+import kinetic.simulator.KineticSimulator;
+import kinetic.simulator.SimulatorConfiguration;
+
+import com.seagate.kinetic.example.openstorage.OpenStorageClient;
+
+/**
+ * Starts (by default) 71 simulators each listens on its own ports and with its
+ * own persistent storage.
+ * <p>
+ * You may require to configure the OS open process/file parameters to run this
+ * program.
+ * 
+ * @see OpenStorageClient
+ * @author chiaming
+ */
+public class SimulatorDrives {
+
+    public static final int MAX_SIMULATOR = 2;
+
+    private int maxSimulator = MAX_SIMULATOR;
+
+    // base port number
+    private int port = 18920;
+
+    // base ssl port
+    private int sslPort = 18990;
+
+    public SimulatorDrives(int maxSimulator, int portBase, int sslportBase) {
+        this.maxSimulator = maxSimulator;
+
+        this.port = portBase;
+
+        this.sslPort = sslportBase;
+    }
+
+    public void run() {
+
+        // simulator instances holder
+        KineticSimulator simulators[] = new KineticSimulator[maxSimulator];
+
+        for (int i = 0; i < maxSimulator; i++) {
+
+            // instantiate a new instance of configuration object
+            SimulatorConfiguration config = new SimulatorConfiguration();
+            // config.setStartSsl(false);
+            // config.setUseMemoryStore(true);
+
+            // set service ports to the configuration
+            int myport = port + i;
+            int mySslPort = sslPort + i;
+            config.setPort(myport);
+            config.setSslPort(mySslPort);
+
+            // set kinetic home for each drive
+            String kineticHome = System.getProperty("user.home")
+                    + File.separator + "kinetic" + File.separator + "instance_"
+                    + myport;
+
+            // set persist store home folder for each instance
+            config.put(SimulatorConfiguration.KINETIC_HOME, kineticHome);
+
+            // start the simulator instance
+            simulators[i] = new KineticSimulator(config);
+
+            System.out.println("\n " + i + ": started simulator. port="
+                    + config.getPort() + ", ssl port=" + config.getSslPort()
+                    + "\n");
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // use bdb store
+        // System.setProperty("kinetic.db.class",
+        // "com.seagate.kinetic.simulator.persist.bdb.BdbStore");
+        int maxSimulator = 2;
+        if (args.length >= 1) {
+            maxSimulator = Integer.parseInt(args[0]);
+        }
+
+        int portBase = 18920;
+        if (args.length >= 2) {
+            portBase = Integer.parseInt(args[1]);
+        }
+
+        int sslportBase = 18990;
+        if (args.length >= 3) {
+            sslportBase = Integer.parseInt(args[2]);
+        }
+
+        // max number of simulators to instantiate.
+        SimulatorDrives vdrives = new SimulatorDrives(maxSimulator, portBase,
+                sslportBase);
+        // start the simulator
+        vdrives.run();
+    }
+
+}
